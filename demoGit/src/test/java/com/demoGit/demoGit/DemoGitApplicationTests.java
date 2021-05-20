b54c9ce0c05e08aac8c967018b448ab36f9cb079
user <user@localhost>
2021-05-20 09:19:49 +0000

    Initial commit

---
+++ demoGit/src/test/java/com/demoGit/demoGit/DemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoGit.demoGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
